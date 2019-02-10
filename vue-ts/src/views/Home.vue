ab38c9406e3f0b190481c3592fcae9f808aa2b10
igayamaguchi <iganin2@gmail.com>
2019-02-10 16:59:38 +0900

    add base

--- vue-ts/src/views/Home.vue
+++ vue-ts/src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <form @submit.prevent="add">
+      <label>
+        <input type="text" v-model="newTodo" />
+      </label>
+      <button type="button">add</button>
+    </form>
+    <ul>
+      <li v-for="todo in todoList" :key="todo.id">{{ todo.value }}</li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {
-    HelloWorld
+@Component({})
+export default class Home extends Vue {
+  newTodo: string = '';
+  todoList: Todo[] = [];
+
+  // @Prop({ required: true }) f: string;
+
+  add(): void {
+    const newId = getNewId(this.todoList);
+    const todo = new Todo(newId, this.newTodo);
+    this.todoList.push(todo);
+    this.newTodo = '';
   }
-})
-export default class Home extends Vue {}
+}
+
+function getNewId(list: Todo[]) {
+  const max = list.reduce((prev, current) => {
+    return prev > current.id ? prev : current.id;
+  }, 0);
+  return max + 1;
+}
+
+class Todo {
+  id: number;
+  value: string;
+  order: number;
+
+  constructor(id: number, value: string, order: number = 99) {
+    this.id = id;
+    this.value = value;
+    this.order = order;
+  }
+}
 </script>
