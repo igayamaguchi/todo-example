6fa3e269a3fb3434f5573d8bdb8ec6606add6fb6
igayamaguchi <iganin2@gmail.com>
2019-02-11 22:55:10 +0900

    fix lint & todo, doing, complete

--- vue-ts/src/views/Home.vue
+++ vue-ts/src/views/Home.vue
@@ -6,8 +6,30 @@
       </label>
       <button type="button">add</button>
     </form>
+    <h3>Todo</h3>
     <ul>
-      <li v-for="todo in todoList" :key="todo.id">{{ todo.value }}</li>
+      <li v-for="todo in todoList" :key="todo.id">
+        <button @click.prevent="doing(todo)" type="button">Complete</button>
+        <div>{{ todo.value }}</div>
+        <button @click.prevent="removeTodo(todo)" type="button">Remove</button>
+      </li>
+    </ul>
+    <h3>Doing</h3>
+    <ul>
+      <li v-for="todo in doingList" :key="todo.id">
+        <button @click.prevent="complete(todo)" type="button">Complete</button>
+        <div>{{ todo.value }}</div>
+        <button @click.prevent="removeDoing(todo)" type="button">Remove</button>
+      </li>
+    </ul>
+    <h3>Complete</h3>
+    <ul>
+      <li v-for="todo in completeList" :key="todo.id">
+        <div>{{ todo.value }}</div>
+        <button @click.prevent="removeComplete(todo)" type="button">
+          Remove
+        </button>
+      </li>
     </ul>
   </div>
 </template>
@@ -19,6 +41,8 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class Home extends Vue {
   newTodo: string = '';
   todoList: Todo[] = [];
+  doingList: Todo[] = [];
+  completeList: Todo[] = [];
 
   // @Prop({ required: true }) f: string;
 
@@ -27,6 +51,33 @@ export default class Home extends Vue {
     const todo = new Todo(newId, this.newTodo);
     this.todoList.push(todo);
     this.newTodo = '';
+  }
+
+  removeTodo(todo: Todo): void {
+    const index = this.todoList.indexOf(todo);
+    this.todoList.splice(index, 1);
+  }
+
+  removeDoing(todo: Todo): void {
+    const index = this.doingList.indexOf(todo);
+    this.doingList.splice(index, 1);
+  }
+
+  removeComplete(todo: Todo): void {
+    const index = this.completeList.indexOf(todo);
+    this.completeList.splice(index, 1);
+  }
+
+  doing(todo: Todo): void {
+    const doing = todo.next();
+    this.doingList.push(doing);
+    this.removeTodo(todo);
+  }
+
+  complete(todo: Todo): void {
+    const complete = todo.next();
+    this.completeList.push(complete);
+    this.removeDoing(todo);
   }
 }
 
@@ -37,7 +88,7 @@ function getNewId(list: Todo[]) {
   return max + 1;
 }
 
-class Todo {
+abstract class AbstractTodo {
   id: number;
   value: string;
   order: number;
@@ -46,6 +97,26 @@ class Todo {
     this.id = id;
     this.value = value;
     this.order = order;
+  }
+
+  abstract next(): AbstractTodo;
+}
+
+class Todo extends AbstractTodo {
+  next(): AbstractTodo {
+    return new Doing(this.id, this.value, this.order);
+  }
+}
+
+class Doing extends AbstractTodo {
+  next(): AbstractTodo {
+    return new Complete(this.id, this.value, this.order);
+  }
+}
+
+class Complete extends AbstractTodo {
+  next(): AbstractTodo {
+    return new Doing(this.id, this.value, this.order);
   }
 }
 </script>
